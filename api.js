fe4eda1ebc3a6ced4f569d0c23fdc36cafa2dc48
Endosmile <kolarovmunari@gmail.com>
2026-07-30 07:47:47 +0100

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 
 const API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:5000/api'
-  : 'https://YOUR-BACKEND-URL.onrender.com/api';
+  : 'https://https://ips-system-thf7.onrender.com/api';
 
 const Api = {
   getToken() {
